9c619967303d3ce55465731b4d9b3547e71194d9
iteye <iteyelmp@gmail.com>
2025-01-10 10:35:20 +0800

    remove code

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 const { program } = require('commander');
 program.version(require('./package.json').version);
 
-const { create, refund, upload, remove, setDefault, download } = require("./src");
+const { create, upload, remove, setDefault, download } = require("./src");
 
 program
     .option('-p, --privateKey [privateKey]', 'private key')
@@ -24,18 +24,6 @@ program
     .action(() => {
         const opts = program.opts();
         create(opts.privateKey, opts.chainId, opts.rpc);
-    });
-
-program
-    .command('refund')
-    .description('refund cost')
-    .option('-p, --privateKey <privateKey>', 'private key')
-    .option('-a, --address <address>', 'flat directory address')
-    .option('-c, --chainId [chainId]', 'chain id')
-    .option('-r, --rpc [rpc]', 'provider url')
-    .action(() => {
-        const opts = program.opts();
-        refund(opts.privateKey, opts.address, opts.rpc, opts.chainId);
     });
 
 program
